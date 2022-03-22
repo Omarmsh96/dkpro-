2fe4d334bb4b92b77b1ffe21161dfd0b9f5fda8f
Omarmsh96 <78327864+Omarmsh96@users.noreply.github.com>
2022-03-22 09:17:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+from cassis import *
+
+with open('TypeSystem.xml', 'rb') as f:
+    typesystem = load_typesystem(f)
+
+with open('essay_en.txt.xmi', 'rb') as f:
+    cas = load_cas_from_xmi(f, typesystem=typesystem)
+
+for sentence in cas.select('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence'):
+    for token in cas.select_covered('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token', sentence):
+        st.write(token.get_covered_text())
+
+        # Annotation values can be accessed as properties
+        st.write('Token: id= {}, lemma={}, pos={}, stem= {}'.format(token.begin, token.end, token.id, token.pos,token.lemma, token.stem))
